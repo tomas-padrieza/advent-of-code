ea43a5d1fe552ec11ee746f29810749117425448
Tomas Padrieza <tomas.padrieza@gmail.com>
2023-12-07 19:44:58 +0200

    day7 - part2

--- src/day7/index.ts
+++ src/day7/index.ts
@@ -33,7 +33,7 @@ const goA = (input: string) => {
 
   const biddings = {}
 
-  input.split(/\n/).forEach((line, idx) => {
+  input.split(/\n/).forEach((line) => {
     const [hand, bid] = line.split(/\s/)
 
     const cards = hand.split("").map((card) => rankedCards[card])
@@ -46,7 +46,7 @@ const goA = (input: string) => {
     })
 
     const occurences = Object.values(paired)
-      .map((pair: number[]) => pair.length)
+      .map((pair: string[]) => pair.length)
       .sort()
       .reverse()
 
@@ -58,15 +58,6 @@ const goA = (input: string) => {
     if (occurences[0] === 3 && occurences[1] === 1) type = "three_of_a_kind"
     if (occurences[0] === 2 && occurences[1] === 2) type = "two_pair"
     if (occurences[0] === 2 && occurences[1] === 1) type = "one_pair"
-
-    // console.log({
-    //   hand,
-    //   cards: cards.join(""),
-    //   paired,
-    //   occurences,
-    //   type,
-    //   bid: Number(bid),
-    // })
 
     games[type].push(cards.join(""))
   })
@@ -82,7 +73,82 @@ const goA = (input: string) => {
 }
 
 const goB = (input: string) => {
-  return
+  const rankedCards = {
+    A: "a",
+    K: "b",
+    Q: "c",
+    T: "d",
+    "9": "e",
+    "8": "f",
+    "7": "g",
+    "6": "h",
+    "5": "i",
+    "4": "j",
+    "3": "k",
+    "2": "l",
+    J: "m",
+  }
+
+  const games = {
+    five_of_a_kind: [],
+    four_of_a_kind: [],
+    full_house: [],
+    three_of_a_kind: [],
+    two_pair: [],
+    one_pair: [],
+    high_card: [],
+  }
+
+  const biddings = {}
+
+  input.split(/\n/).forEach((line, idx) => {
+    const [hand, bid] = line.split(/\s/)
+
+    const cards = hand.split("").map((card) => rankedCards[card])
+    biddings[cards.join("")] = Number(bid)
+
+    const paired = {}
+
+    cards.forEach((card) => {
+      const cardKey = card === "m" ? "JOKER" : card
+      paired[cardKey] = paired[cardKey]
+        ? [...paired[cardKey], cardKey]
+        : [cardKey]
+    })
+
+    const jokers = Object.values(paired)
+      .filter(([first]: string[]) => first === "JOKER")
+      .flat().length
+
+    const occurences = Object.values(paired)
+      .filter(([first]: string[]) => first !== "JOKER")
+      .map((pair: string[]) => pair.length)
+      .sort()
+      .reverse()
+
+    if (occurences.length === 0) occurences[0] = 0
+
+    occurences[0] += jokers
+
+    let type = "high_card"
+
+    if (occurences[0] === 5) type = "five_of_a_kind"
+    if (occurences[0] === 4) type = "four_of_a_kind"
+    if (occurences[0] === 3 && occurences[1] === 2) type = "full_house"
+    if (occurences[0] === 3 && occurences[1] === 1) type = "three_of_a_kind"
+    if (occurences[0] === 2 && occurences[1] === 2) type = "two_pair"
+    if (occurences[0] === 2 && occurences[1] === 1) type = "one_pair"
+
+    games[type].push(cards.join(""))
+  })
+
+  const keys = Object.keys(games)
+
+  const ranks = []
+  for (let key of keys)
+    ranks.push(...games[key].sort().map((card) => biddings[card]))
+
+  return ranks.reverse().reduce((acc, bid, idx) => acc + bid * (idx + 1), 0)
 }
 
 /* Results */
